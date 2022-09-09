fd414c8cfb4fdece6105119d8c3165fedeec45eb
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-09 13:42:17 +0530

    Adding program for adding employee to the address book

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AddressBookDay36;
+
 Console.WriteLine("Wellcome to the Address Book Problem");
+
+AddressBookUC obj = new AddressBookUC();
+obj.AddNewContact();
+AddressBookUC.PrintAddressBook();
