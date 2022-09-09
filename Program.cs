f0c1cb4d37cc7e209f26a2518eb47e557ab45a84
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-09 13:46:42 +0530

    Adding Program for editing the existing contact person using their name

--- Program.cs
+++ Program.cs
@@ -4,5 +4,10 @@ using AddressBookDay36;
 Console.WriteLine("Wellcome to the Address Book Problem");
 
 AddressBookUC obj = new AddressBookUC();
-obj.AddNewContact();
+//obj.AddNewContact();
+//AddressBookUC.PrintAddressBook();
+
+Console.WriteLine("Enter the FirstName to Edit");
+string firstName = Console.ReadLine();
+obj.EditContact(firstName);
 AddressBookUC.PrintAddressBook();
