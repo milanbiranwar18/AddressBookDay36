dd5d03dc403bab1289bcefa726be79f650f28901
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-09 13:39:19 +0530

    Program for adding employee to the address book

---
+++ AddressBookUC.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookDay36
+{
+    public class AddressBookUC
+    {
+        public static List<Contact> People = new List<Contact>();
+
+        public void AddNewContact()
+        {
+            //Console.WriteLine("Enter the country you want to add address");
+            var addressBook = Console.ReadLine();
+
+            Contact person = new Contact();
+
+            Console.Write("Enter First Name: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Address: ");
+            person.Address = Console.ReadLine();
+
+
+            Console.Write("Enter City: ");
+            person.City = Console.ReadLine();
+
+            Console.Write("Enter ZipCode: ");
+            person.ZipCode = Console.ReadLine();
+
+
+            Console.Write("Enter Phone Number: ");
+            person.PhoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            person.Email = Console.ReadLine();
+        }
+    }
+    
+}
